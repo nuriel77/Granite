9ce6fe56bf8550a2c151704e6dcb78e5598b109a
Nuriel Shem-Tov <nuriel.shemtov@clustervision.com>
2014-01-31 15:45:50 +0100

    Configured log config and application config loaders and module dynamic loader

--- lib/Granite/Modules/Schedulers.pm
+++ lib/Granite/Modules/Schedulers.pm
@@ -1,10 +1,16 @@
 package Granite::Modules::Schedulers;
 use strict;
 use warnings;
-use Module::Load;
+use Granite::Utils::ModuleLoader;
 
-sub load_scheduler_api {
-    1;
+sub init_scheduler_module {
+    my $scheduler = $_[1];
+
+    if ( my $error = Granite::Utils::ModuleLoader::load_module( $scheduler ) ){ 
+        # Failed to load
+        return $error;
+    }   
+
 }
 
 1;
