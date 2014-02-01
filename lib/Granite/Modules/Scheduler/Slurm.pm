ae3da129d20944fb245b0739ecf52d78286a2da7
Nuriel Shem-Tov <nuriel.shemtov@clustervision.com>
2014-02-01 10:30:07 +0100

    Added queue names(partitions) adding metadata attr to scheduler object

--- lib/Granite/Modules/Scheduler/Slurm.pm
+++ lib/Granite/Modules/Scheduler/Slurm.pm
@@ -6,25 +6,41 @@ use Slurm qw(:constant);
 use Carp 'confess';
 use Moose;
     with 'Granite::Modules::Scheduler';
+use namespace::autoclean;
+
+has slurm => ( is => 'rw', isa => 'Object' );
+
+around 'new' => sub {
+    my $orig = shift;
+    my $class = shift;
+    my $self = $class->$orig(@_);
+
+    my $slurm_conf = $self->metadata->{config_file};
+    $self->slurm ( Slurm::new($slurm_conf) );
+    confess "Slurm error: " . $self->slurm->strerror() . "\n"
+        if $self->slurm->get_errno();
+
+    return $self;    
+};
 
 sub get_queue {
-    my $slurm_conf = '/opt/slurm/etc/slurm.conf';
-    my $slurm = Slurm::new($slurm_conf);
-    my $default_partition = '_root_';
+    my $self = shift;
+    my $reservation_queue = $self->metadata->{reservation_queue};
 
-    confess "Slurm error: " . $slurm->strerror() . "\n"
-        if $slurm->get_errno();
-
-    my $sq = $slurm->load_jobs();
+    my $sq = $self->slurm->load_jobs();
 
     my $output;
     for (@{$sq->{job_array}}){
-        #print $_->{job_id} . ", prio = $_->{priority}, num_cpus = $_->{num_cpus}, num_nodes = $_->{num_nodes}\n"
          $output .= '' . ( join ":", ( $_->{job_id}, $_->{priority}, $_->{num_cpus}, $_->{num_nodes}) ) . ';'
-            if $_->{partition} eq $default_partition;
+            if $_->{partition} eq $reservation_queue;
     }
     return $output;
 }
 
+sub get_nodes {
+    
+}
+
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
 
 1;
