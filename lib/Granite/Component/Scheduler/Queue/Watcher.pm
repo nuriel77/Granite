fd804568377d0a0c7aa1d2594ce8487ab244d02f
Nuriel Shem-Tov <nuriel.shemtov@clustervision.com>
2014-02-03 00:19:18 +0100

    temp: attempt add queue processing

--- lib/Granite/Component/Scheduler/Queue/Watcher.pm
+++ lib/Granite/Component/Scheduler/Queue/Watcher.pm
@@ -3,13 +3,14 @@ use strict;
 use warnings;
 use Data::Dumper::Concise;
 use POE;
-use vars qw($log $debug $scheduler);
+use vars qw($log $debug $scheduler $queue);
 
 our $child_max = 1;
 
 sub run {
     ( $log, $debug )      = @_[ ARG0..ARG2 ];
     my ( $kernel, $heap ) = @_[ KERNEL, HEAP ];
+
     $scheduler            = $heap->{scheduler};
 
     $log->debug('[ ' . $_[SESSION]->ID() . ' ] Initializing ' . __PACKAGE__)
@@ -23,9 +24,12 @@ sub run {
                 _start => \&parent_start,
                 _stop  => \&parent_stop,
                 _child => \&parent_spawn_child,
+                _default => \&Granite::Engine::handle_default,
                 result => \&parent_got_result,
             },
-            heap => { scheduler => $scheduler->{(keys %{$scheduler})[0]} }
+            heap => {
+                scheduler => $scheduler->{(keys %{$scheduler})[0]},
+            }
         );
     
     $log->debug( '[ ' . $_[SESSION]->ID()
@@ -39,7 +43,7 @@ sub parent_start {
 }
 
 sub parent_spawn_child {
-    my ($heap, $operation, $child) = @_[HEAP, ARG0, ARG1];
+    my ($heap, $kernel, $operation, $child) = @_[HEAP, KERNEL, ARG0, ARG1];
  
     # TODO: Create main class to handle any event management
     if ($operation eq 'create' or $operation eq 'gain') {
@@ -62,8 +66,9 @@ sub parent_spawn_child {
             $log->debug( '[ ' . $_[SESSION]->ID . ' ] Spawning a new child session in '
                         . $heap->{scheduler}->{metadata}->{reservation_flush_interval}
                         . ' seconds');
-            sleep $heap->{scheduler}->{metadata}->{reservation_flush_interval};
-            &create_child;
+            $kernel->delay('_start' => $heap->{scheduler}->{metadata}->{reservation_flush_interval} || 10 );
+            #sleep $heap->{scheduler}->{metadata}->{reservation_flush_interval};
+            #&create_child;
         }
     }
 }
@@ -88,16 +93,28 @@ sub create_child {
 
 }
 
-
 sub parent_got_result {
     my ($kernel, $session, $heap) = @_[KERNEL, SESSION, HEAP];
-    my ( $sessionId, $data ) = @_[ARG0,ARG1];
+    my ( $sessionId, $data ) = @_[ ARG0, ARG1 ];
 
     $log->debug('[ ' . $_[SESSION]->ID()
                 . ' ] Save queue data from child ID [ '
                 . $sessionId . ' ]' )
         if $debug;
+
     # Save data, or pass back to engine.
+    $heap->{scheduler}->{queue} = { by => $sessionId, data => $data };
+
+    my $reservation_queue = $heap->{scheduler}->{metadata}->{reservation_queue};
+
+    my $in_reservation_queue = grep { $_->{partition} eq $reservation_queue } @{$data};
+
+    my $sess = $_[KERNEL]->alias_resolve('engine');
+    $_[KERNEL]->post( $sess , 'process_res_q' );
+    #warn "$in_res jobs in reservation queue\n";
+         #$output .= '' . ( join ":", ( $_->{job_id}, $_->{priority}, $_->{num_cpus}, $_->{num_nodes}) ) . ';'
+         #   if $_->{partition} eq $reservation_queue;
+
 }
 
 sub child_process_input {
@@ -107,9 +124,9 @@ sub child_process_input {
 
     if ( ref $queue_data eq 'ARRAY' && @{$queue_data} > 0 ) {
         $log->info('[ ' . $_[SESSION]->ID . ' ] Have ' . scalar @{$queue_data}
-                   . ' item(s) in the queue. Posting to parent. Child terminating.');
-        $log->debug("[ " . $_[SESSION]->ID . " ] Have queue data: " . Dumper $queue_data )
-            if $debug;
+                   . ' jobs(s) from all queues. Posting to parent. Child terminating.');
+        #$log->debug("[ " . $_[SESSION]->ID . " ] Have queue data: " . Dumper $queue_data )
+        #    if $debug;
 
         $kernel->post($heap->{parent}, 'result', $_[SESSION]->ID(), $queue_data);
     }
@@ -117,6 +134,5 @@ sub child_process_input {
         $log->info("[ " . $_[SESSION]->ID . " ] Queue empty.");
     }
 }
-
 
 1;
