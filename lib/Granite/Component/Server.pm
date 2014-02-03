feb0d86cbaa9c12ce9607770417759ce4edabfb9
Nuriel Shem-Tov <nuriel.shemtov@clustervision.com>
2014-02-03 12:46:03 +0100

    Added moose attribute type verification

--- lib/Granite/Component/Server.pm
+++ lib/Granite/Component/Server.pm
@@ -3,58 +3,134 @@ use strict;
 use warnings;
 use Socket;
 use Cwd 'getcwd';
+use File::Basename 'dirname';
+use Data::Validate::IP qw(is_ipv4 is_ipv6);
 use Scalar::Util 'looks_like_number';
 use Sys::Hostname;
 use Data::Dumper;
 use POE
     qw( Wheel::SocketFactory Driver::SysRW Filter::Stream Wheel::ReadWrite );
 
+use Moose::Util::TypeConstraints;
 use Moose;
     with 'Granite::Component::Server::SSLify';
 use namespace::autoclean;
 
 use vars
-    qw( $log $port $granite_key $granite_crt $debug
-        $bind $max_clients $client_filters $granite_cacrt
-        $granite_verify_client $granite_cipher $granite_crl
-        $client_namespace $host_name $disable_ssl $unix_socket );
+    qw( $log $granite_key $granite_crt $debug $client_filters
+        $granite_cacrt $granite_verify_client $granite_cipher
+        $granite_crl $client_namespace $host_name $disable_ssl
+        $unix_socket $self );
 
-before 'run' => sub {
-    $port            = $Granite::cfg->{server}->{port}              || 21212;
-    $bind            = $Granite::cfg->{server}->{bind}              || '127.0.0.1';
-    $max_clients     = $Granite::cfg->{server}->{max_clients}       || 10;
-    $host_name       = $Granite::cfg->{server}->{hostname}          || hostname();
+=head1 DESCRIPTION
+
+Granite server component
+features tcp, unix sockets
+and socket SSL 
+
+=head2 CONSTRAINTS
+
+Enumeration and type constraints
+via Moose::Util::TypeConstraints
+
+=head3 subtype 'Port'
+
+'Port' can be Int range 1025..65535
+
+=cut
+
+subtype 'Port',
+    as 'Int',
+    where { $_ > 1024 && $_ <= 65535 };
+
+
+=head3 subtype 'BindAddress'
+
+'BindAddress' can be IPv4 or IPv6
+
+=cut
+
+subtype 'BindAddress',
+    as 'Str',
+    where { is_ipv4($_) || is_ipv6($_) };
+
+
+=head2 ATTRIBUTES
+
+L<port>
+
+L<bind>
+
+L<unix_socket>
+
+L<max_clients>
+
+L<host_name>
+
+=cut
+
+has port        => ( is => 'rw', isa => 'Port',        clearer   => '_undef_port',        predicate => '_has_port', default => 21212 );
+has bind        => ( is => 'rw', isa => 'BindAddress', clearer   => '_undef_bind',        predicate => '_has_bind', default => '127.0.0.1' );
+has unix_socket => ( is => 'rw', isa => 'UnixSocket',  clearer   => '_undef_unix_socket', predicate => '_has_unix_socket', required => 0);
+has max_clients => ( is => 'rw', isa => 'Int', default => 10 );
+has host_name   => ( is => 'rw', isa => 'Str', default => hostname() );
+
+
+=head2 METHODS
+
+=head3 BUILD
+
+Assign class parameters before 'run'
+
+=cut
+
+sub BUILD {
+
+    $self = shift;
+
+    $self->port            ( $Granite::cfg->{server}->{port} );
+    $self->bind            ( $Granite::cfg->{server}->{bind} );
+    $self->host_name       ( $Granite::cfg->{server}->{hostname}    );
+    $self->max_clients     ( $Granite::cfg->{server}->{max_clients} );
+    $self->unix_socket     ( $Granite::cfg->{server}->{unix_socket} )
+        if $Granite::cfg->{server}->{unix_socket};
+
     $granite_crt     = $Granite::cfg->{server}->{cert}   ? getcwd . '/' . $Granite::cfg->{server}->{cert}   : undef;
     $granite_key     = $Granite::cfg->{server}->{key}    ? getcwd . '/' . $Granite::cfg->{server}->{key}    : undef;
     $granite_cacrt   = $Granite::cfg->{server}->{cacert} ? getcwd . '/' . $Granite::cfg->{server}->{cacert} : undef;
     $granite_crl     = $Granite::cfg->{server}->{crl}    ? getcwd . '/' . $Granite::cfg->{server}->{crl}    : undef;
-    $unix_socket     = $Granite::cfg->{server}->{unix_socket}       || undef;
     $granite_cipher  = $Granite::cfg->{server}->{cipher}            ||'DHE-RSA-AES256-GCM-SHA384:AES256-SHA';
 
     $ENV{GRANITE_CLIENT_CERTIFICATE} = 1 if $ENV{GRANITE_VERIFY_CLIENT};
     $Granite::cfg->{server}->{client_certificate} ||= $Granite::cfg->{server}->{verify_client}; 
+
+    return $self;
 };
+
+
+=head3 __PACKAGE__->run( $parent_sessionId )
+
+Method 'run' will start the server session
+
+=cut
 
 sub run {
 
-    ($log, $debug) = @_[ ARG0, ARG1 ];
+    ( $log, $debug ) = ( $Granite::log, $Granite::debug );
+    $self = shift;
+    my $sessionId  = shift;
 
-    $log->debug('[ ' . $_[SESSION]->ID() . ' ] Initializing Granite::Component::Server')
+    $log->debug('[ ' . $sessionId . ' ] Initializing Granite::Component::Server')
         if $debug;
 
-    # Fork server worker
-    # ==================
-    POE::Kernel->stop();
-    $log->debug( '[ ' . $_[SESSION]->ID() . ' ] Server Worker forked' );
-
     $disable_ssl = $ENV{GRANITE_DISABLE_SSL} || $Granite::cfg->{server}->{disable_ssl};
-    if ( $unix_socket
+    if ( $self->_has_unix_socket
         && ( $Granite::cfg->{server}->{port} || $Granite::cfg->{server}->{bind} )
     ){
-        $log->warn('[ ' . $_[SESSION]->ID() . ' ] Warning: Both unix socket and tcp options are configured.'
+        $log->warn('[ ' . $sessionId . ' ] Warning: Both unix socket and tcp options are configured.'
                   . ' Unix socket takes precedence.');
-        $bind = undef;
-        $port = undef;
+        $self->undef_bind;
+        $self->undef_port;
     }  
 
     $log->logcroak("Missing certificate file definition")   if !$disable_ssl && !$granite_crt;
@@ -62,14 +138,14 @@ sub run {
 
     # Set global SSL options
     # ======================
-    unless ( $disable_ssl or $unix_socket ){
-        $log->debug('[ ' . $_[SESSION]->ID() . ' ] Setting SSLify options') if $debug;
+    unless ( $disable_ssl or $self->_has_unix_socket ){
+        $log->debug('[ ' . $sessionId . ' ] Setting SSLify options') if $debug;
         sslify_options( $granite_key, $granite_crt, $granite_cacrt );
     }
 
     # Check access to unix socket
     # ===========================
-    if ( $unix_socket && -e $unix_socket ){
+    if ( $self->_has_unix_socket && -e $unix_socket ){
         $log->logdie("Access denied on '$unix_socket'. Check permissions.")
         	unless -w $unix_socket;
         unlink $unix_socket or $log->logdie("Cannot unlink old socket '$unix_socket': $!");
@@ -81,10 +157,10 @@ sub run {
                 my ( $heap, $kernel ) = @_[ HEAP, KERNEL ];
                 $_[KERNEL]->alias_set('server');
                 $heap->{server_wheel} = POE::Wheel::SocketFactory->new(
-                    SocketDomain => $unix_socket ? PF_UNIX : AF_INET,
-                    BindAddress  => $unix_socket || $bind,
-                    BindPort     => ( $unix_socket ? undef : $port ),
-                    ListenQueue  => $max_clients,
+                    SocketDomain => $self->_has_unix_socket ? PF_UNIX : AF_INET,
+                    BindAddress  => $self->_has_unix_socket || $self->bind,
+                    BindPort     => ( $self->_has_unix_socket ? undef : $self->port ),
+                    ListenQueue  => $self->max_clients,
                     Reuse        => 'yes',
                     SuccessEvent => 'client_accept',
                     FailureEvent => 'server_error',
@@ -100,20 +176,23 @@ sub run {
             _default          => \&Granite::Engine::handle_default,
             _stop             => \&server_error,
         }
-    ) or $log->logcroak('[ ' . $_[SESSION]->ID() .  " ] can't POE::Session->create: $!" );
+    ) or $log->logcroak('[ ' . $sessionId .  " ] can't POE::Session->create: $!" );
 
-    if ( $unix_socket ){ 
-        $log->info('[ ' . $_[SESSION]->ID() .  " ] Server started at socket '$unix_socket' with session ID: " . $session->ID() );
+    if ( $self->_has_unix_socket ){ 
+        $log->info('[ ' . $sessionId .  " ] Server started at socket '" . $self->unix_socket . "' with session ID: " . $session->ID() );
     }
     else {
-        $log->info('[ ' . $_[SESSION]->ID() .  " ] Server started at $bind:$port with session ID: " . $session->ID() );
+        $log->info('[ ' . $sessionId .  ' ] Server started at ' . $self->bind . ':' . $self->port . ' with session ID: ' . $session->ID() );
     }
 
-    POE::Kernel->run();
-    return;
-
-
 }
+
+
+=head3 server_error
+
+Handler for SocketFactory FailureEvent
+
+=cut
 
 sub server_error {
     my ($operation, $errnum, $errstr ) = @_[ARG0..ARG2];
@@ -124,6 +203,13 @@ sub server_error {
                 . $_[SENDER]->ID() . ( $errnum ? ": ($errnum) $errstr" : '' ) )
         if looks_like_number($_[SENDER]->ID());
 }
+
+
+=head3 _client_error
+
+Handler for client errors
+
+=cut
 
 sub _client_error {
     my ( $kernel, $heap, $operation ) = @_[ KERNEL, HEAP, ARG0 ];
@@ -138,6 +224,15 @@ sub _client_error {
     delete $_[HEAP]{wheels}{$wheel_id};
 }
 
+
+=head3 _close_delayed
+
+Disconnect a client
+call to this handler is typically 
+delayed about 1 second
+
+=cut
+
 sub _close_delayed {
     my ( $kernel, $heap, $wheel_id ) = @_[ KERNEL, HEAP, ARG0 ];
 
@@ -149,6 +244,15 @@ sub _close_delayed {
     $log->info('[ ' . $_[SESSION]->ID() . ' ]->(' . $wheel_id . ") Client disconnected.");
 }
 
+
+=head3 _client_disconnect
+
+Handler for client disconnect
+Will delay termination in
+one second and call _close_delayed
+
+=cut
+
 sub _client_disconnect {
     my ( $heap, $kernel, $wheel_id ) = @_[ HEAP, KERNEL, ARG0 ];
 
@@ -158,6 +262,18 @@ sub _client_disconnect {
     $kernel->delay( close_delayed => 1, $wheel_id )
       unless ( $heap->{server}->{$wheel_id}->{disconnecting}++ );
 }
+
+
+=head3 _client_input
+
+client input handler for all 
+client input data.
+
+If first input for a client:
+we run client verification and register
+the client if authentication is successful
+
+=cut
 
 sub _client_input {
     my ( $heap, $kernel, $input, $wheel_id ) = @_[ HEAP, KERNEL, ARG0, ARG1 ];
@@ -186,12 +302,19 @@ sub _client_input {
     }
 }
 
+
+=head3 _client_accept
+
+New socket connection established
+
+=cut
+
 sub _client_accept {
     my ( $heap, $kernel, $socket, $wheel_id ) = @_[ HEAP, KERNEL, ARG0, ARG1 ];
 
     $log->info('[ ' . $_[SESSION]->ID() .' ] New connection received');
 
-    unless ( $disable_ssl or $unix_socket ){
+    unless ( $disable_ssl or $self->_has_unix_socket ){
         unless ( $socket = sslify_socket( $socket, $granite_crl, $_[SESSION]->ID()) ){
             delete $heap->{server}->{$wheel_id}->{wheel};
             return undef;
@@ -216,6 +339,13 @@ sub _client_accept {
     $heap->{server}->{ $io_wheel->ID() }->{wheel}  = $io_wheel;
     $heap->{server}->{ $io_wheel->ID() }->{socket} = $socket;
 }
+
+
+=head3 _verify_client
+
+Verify the client when connected
+
+=cut
 
 sub _verify_client {
     my ( $heap, $kernel, $input, $wheel_id, $canwrite ) 
@@ -269,6 +399,13 @@ sub _verify_client {
 
 }
 
+
+=head3 _sanitize_input
+
+Do not accept input with invalid characters
+
+=cut
+
 sub _sanitize_input {
     my ($sessionId, $wheel_id, $input) = @_;
 
@@ -283,6 +420,13 @@ sub _sanitize_input {
     }
     return $input;
 }
+
+
+=head3 _get_remote_address
+
+Get the IP:Port of the client
+
+=cut
 
 sub _get_remote_address {
     my ($socket, $sessionId, $wheel_id) = @_;
@@ -304,5 +448,11 @@ sub _get_remote_address {
 }
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
+
+=head1 AUTHOR
+
+Nuriel Shem-Tov
+
+=cut
 
 1;
