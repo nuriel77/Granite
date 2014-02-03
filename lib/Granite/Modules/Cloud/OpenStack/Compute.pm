998df19025898c360f9f700093b601e1b4347188
Nuriel Shem-Tov <nuriel.shemtov@clustervision.com>
2014-02-04 00:14:20 +0100

    Remove stats function from openstack compute subclass, as details method returns all stats

--- lib/Granite/Modules/Cloud/OpenStack/Compute.pm
+++ lib/Granite/Modules/Cloud/OpenStack/Compute.pm
@@ -16,18 +16,31 @@ Subclass of Net::OpenStack::Compute
 
 See http://search.cpan.org/~ironcamel/Net-OpenStack-Compute-1.1002/lib/Net/OpenStack/Compute.pm
 
+=head1 METHODS
+
+=head2 get_hypervisors
+
+Get all hypervisors with details
+
 =cut
 
 sub get_hypervisors {
-    my ($self, %params) = @_;
-    my $q = Net::OpenStack::Compute::_get_query(%params);
-    my $param = (keys %params)[0];
-    my $res = $self->_get($self->_url("/os-hypervisors", $params{$param}, $q));
+    my ($self, $param) = @_;
+    my $res = $self->_get($self->_build_url("/os-hypervisors", $param));
     return from_json($res->content)->{'hypervisors'};
 }
 
-sub get_hypervisors_stats {
-    shift->get_hypervisors(statistics => 1);
+=head2 _build_url
+
+Helper function to construct a generic URL
+
+=cut
+
+sub _build_url {
+    my ($self, $path, $action) = @_;
+    my $url = $self->base_url . $path;
+    $url .= '/' . $action if $action;
+    return $url;
 }
 
 
